89d844abbf105194473089d6ded459fa82bdec40
Lunarixus <Nathan@lunarixus.party>
2020-12-07 22:22:27 +0100

    walleye: Remove useless makefiles
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_walleye.mk \
-    $(LOCAL_DIR)/aosp_walleye_test.mk \
-    $(LOCAL_DIR)/aosp_walleye_hwasan.mk \
+    $(LOCAL_DIR)/aosp_walleye.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_walleye-userdebug \
-    aosp_walleye_test-userdebug
+    aosp_walleye-userdebug
