3e7deb5df3ba9580cce44a2fbcd6533d4e8929c5
PixelBoot <bootingpixels@gmail.com>
2021-04-23 04:55:14 -0700

    walleye: include device build fingerprint

--- aosp_walleye.mk
+++ aosp_walleye.mk
@@ -45,3 +45,9 @@ PRODUCT_BRAND := google
 PRODUCT_NAME := aosp_walleye
 PRODUCT_DEVICE := walleye
 PRODUCT_MODEL := Pixel 2
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=walleye \
+    PRIVATE_BUILD_DESC="walleye-user 11 RP1A.201005.004.A1 6934943 release-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:11/RP1A.201005.004.A1/6934943:user/release-keys
